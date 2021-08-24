78ff6a06474233f6fe78017de760e9a5a3e41921
8345t0n <ekays@hawk.iit.edu>
2021-08-24 09:22:42 -0400

    Yeye

---
+++ Scripts/goals/goalkeeper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Import save file
+# Load data into goal list
+# Main program loop
+    # Display goal data
+    # UI (Add/Del/Chk/Luk)
+        # add <goal name>
+        # del <timeframe> <num>
+        # chk <timeframe> <num>
+        # luk <timeframe> <num>
